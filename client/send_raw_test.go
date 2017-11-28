f679f310a2ef447e54ca0a9047420933a5e81e5a
David Rauschenbach <davidr@imap.cc>
2017-11-28 12:41:47 -0800

    NUB-331 Implement Binary XML client lib with Connect() and Send() support

---
+++ client/send_raw_test.go
@@ -0,0 +1,75 @@
+package binaryxml_client_test
+
+import (
+	"bufio"
+	"bytes"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/BixData/binaryxml"
+	"github.com/BixData/binaryxml/client"
+	"github.com/BixData/binaryxml/messages"
+	"github.com/docktermj/go-logger/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSendRaw(t *testing.T) {
+	logger.SetLevel(logger.LevelDebug)
+	assert := assert.New(t)
+
+	// Create a server
+	listener, err := net.Listen("tcp", "localhost:0")
+	assert.NoError(err)
+	defer listener.Close()
+	port := listener.Addr().(*net.TCPAddr).Port
+	logger.Debugf("Listening on port %d", port)
+
+	var conn net.Conn
+	accepted := false
+	received := false
+	var listenerErr error
+	go func() {
+		if conn, listenerErr = listener.Accept(); listenerErr != nil {
+			return
+		}
+		defer conn.Close()
+		accepted = true
+
+		// Read message
+		reader := bufio.NewReader(conn)
+		var param uint8
+		var binaryXML []byte
+		if listenerErr = binaryxml_messages.ReadMessage(reader, &param, &binaryXML); listenerErr != nil {
+			return
+		}
+		received = true
+
+	}()
+
+	// Create a client
+	client, err := binaryxml_client.Connect("127.0.0.1", port)
+	assert.NoError(err)
+
+	time.Sleep(20 * time.Millisecond)
+	assert.True(accepted)
+
+	// Prepare a BinaryXML request
+	type MyRequest struct {
+		XMLName     struct{} `xml:"BixRequest"`
+		Request     string   `xml:"request"`
+		ToNamespace string   `xml:"toNamespace"`
+	}
+	myRequest := MyRequest{ToNamespace: "foo", Request: "bar"}
+
+	var buffer bytes.Buffer
+	writer := bufio.NewWriter(&buffer)
+	assert.NoError(binaryxml.Encode(myRequest, writer))
+	writer.Flush()
+
+	// Send it
+	assert.NoError(client.SendRaw(0, buffer.Bytes()))
+
+	time.Sleep(200 * time.Millisecond)
+	assert.True(received)
+}
